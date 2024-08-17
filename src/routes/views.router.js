169e52ac93999c54198923b4a73680df18537233
yamil131989 <152355713+yamil131989@users.noreply.github.com>
2024-08-17 00:56:08 -0300

    Update views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,4 +1,22 @@
+const { Router } = require('express')
+const ProductsManagerFs = require('../managers/FileSystem/products.manager')
+
 const router = Router()
 
+const prodManagerFS = new ProductsManagerFs()
 
-module.exports = router
+router.get('/',async (req,res)=>{
+    const productos = await prodManagerFS.getProducts()  
+    //console.log({productos})  
+    return res.render('home',{productos,styles:"styles.css"})
+})
+ 
+router.get('/realtimeproducts',async (req,res)=>{
+    const productos = await prodManagerFS.getProductsWebsocket()  
+    //console.log({productos})  
+    //return res.render('realTimeProducts',{productos,styles:"styles.css"})
+    return res.render('realTimeProducts',{productos,styles:"styles.css"})
+
+})
+
+module.exports = router;
